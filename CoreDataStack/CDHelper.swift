81b9022395529244ac0002e1b79b4473188f5dea
kengo hiwatashi <kengo.hiwatashi@gmail.com>
2016-07-17 19:01:54 +0900

    Initial commit.

---
+++ CoreDataStack/CDHelper.swift
@@ -0,0 +1,14 @@
+//
+//  CDHelper.swift
+//  CoreDataStack
+//
+//  Created by kengo hiwatashi on 7/17/16.
+//  Copyright © 2016 appleplusminus. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CDHelper {
+    
+}
