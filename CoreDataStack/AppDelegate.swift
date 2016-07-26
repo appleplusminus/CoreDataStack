91a41d1b157f22a1a4bbdba933c866ba90c1f234
kengo hiwatashi <kengo.hiwatashi@gmail.com>
2016-07-26 20:10:41 +0900

    Creates the first context.

--- CoreDataStack/AppDelegate.swift
+++ CoreDataStack/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        
+        moc.persistentStoreCoordinator = CDHelper.sharedInstance.coordinator
+        
         return true
     }
 
